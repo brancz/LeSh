4d6b24655d299bae4b668d9a4fa2a34c2c48ea02
flower-pot <flowerpot@pondati.net>
2014-01-21 21:37:27 +0100

    remove features that enables user to choose the link route on their own
    it caused too much trouble for now enough return value

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -36,6 +36,6 @@ class LinksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def link_params
-      params.require(:link).permit(:uri, :short_route)
+      params.require(:link).permit(:uri)
     end
 end
